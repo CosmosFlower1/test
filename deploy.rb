dd9daa21e52570c62fd1775fc34d3c922283b2aa
小杉　将史 <mkosugi@yahoo-corp.jp>
2015-07-29 12:05:36 +0900

    Add AWS Connect

--- deploy.rb
+++ deploy.rb
@@ -3,7 +3,21 @@
 require 'rubygems'
 require 'aws-sdk'
 
-test_val = ENV["TEST_VAL"]
+# AWS に接続する
 
-print test_val
+s3 = Aws::S3::Client.new
+
+# 接続したS3 の buckets のリストを取得
+
+resp = s3.list_buckets
+
+# S3 に登録されている bucket のリストを取得
+
+bucket_list = resp.buckets.map(&:name)
+
+resp2 = s3.list_objects(bucket: ENV["AWS_BUCKET_NAME"])
+
+resp2.contents.each do |object|
+  puts "#{object.key}"
+end
 
