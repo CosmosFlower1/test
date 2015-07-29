188e4bada7b078aa9843567bb735e6fa79b5ce22
小杉　将史 <mkosugi@yahoo-corp.jp>
2015-07-29 13:30:24 +0900

    check dir

--- deploy.rb
+++ deploy.rb
@@ -5,19 +5,25 @@ require 'aws-sdk'
 
 # AWS に接続する
 
-s3 = Aws::S3::Client.new
+#s3 = Aws::S3::Client.new
 
 # 接続したS3 の buckets のリストを取得
 
-resp = s3.list_buckets
+#resp = s3.list_buckets
 
 # S3 に登録されている bucket のリストを取得
 
-bucket_list = resp.buckets.map(&:name)
+#bucket_list = resp.buckets.map(&:name)
 
-resp2 = s3.list_objects(bucket: ENV["AWS_BUCKET_NAME"])
+#resp2 = s3.list_objects(bucket: ENV["AWS_BUCKET_NAME"])
 
-resp2.contents.each do |object|
-  puts "#{object.key}"
-end
+#resp2.contents.each do |object|
+#  puts "#{object.key}"
+#end
 
+
+puts Dir::pwd
+
+
+
+p Dir::entries(Dir::pwd)
